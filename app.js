4e82396fa6b41c3df1e59d0003e7625cb94ef770
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-18 23:26:17 +0530

    Handling POST Requests(Updating json file)

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ app.get('/api/v1/tours', (req, res) => {
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
-  const newID = tours[tours.length - 1] + 1;
+  const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
+
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
