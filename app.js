65c174e455f61dc5d8dbe496e37b17e0c4ae837a
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-19 16:45:22 +0530

    Handling PATCH Requests.

--- app.js
+++ app.js
@@ -65,6 +65,25 @@ app.post('/api/v1/tours', (req, res) => {
   // res.send('Done');
 });
 
+////////////////////////////////
+
+app.patch('/api/v1/tours/:id', (req,res) => {
+
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here..>'
+    }
+  })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
