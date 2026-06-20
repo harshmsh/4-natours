edc9139b13c81ba2026f6576eb7e230260e16f7c
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-20 11:46:31 +0530

    Environment Variables

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
+
+console.log(process.env.NODE_ENV);
+// console.log(process.env.APP_USERNAME);
 
 const port = 3000;
 app.listen(port, () => {
