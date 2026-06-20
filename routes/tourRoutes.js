f86eb80eebefc20a1fed2a364c351ef88e738068
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-20 10:06:35 +0530

    Param Middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 
-const tourController = require('./../controllers/tourController')
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router.param('id', tourController.checkID);
 
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
