3430cfbf8c06d70d22d55755f77060da50217730
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-20 10:50:06 +0530

    Serving static files.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 ///1. MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json()); //here express.json is middleware
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
