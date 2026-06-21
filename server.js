54a0a3f0ceeccd186b6e20897169880e4fca156d
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-21 17:24:59 +0530

    Connecting Express app to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+const dns = require('dns');
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 console.log(process.env.NODE_ENV);
 // console.log(process.env.APP_USERNAME);
