c186f8197f5e1b17ae02d3fd84a7fcf5b55b2cfc
harshmsh <harshmaheshwari1110@gmail.com>
2026-06-24 18:05:49 +0530

    Importing Development data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,60 @@
+const dns = require('dns');
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// read json file
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// mport data into db
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//delete all data from db
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data deleted successfully!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import'){
+  importData();
+} else if (process.argv[2] === '--delete'){
+  deleteData();
+}
+console.log(process.argv);
